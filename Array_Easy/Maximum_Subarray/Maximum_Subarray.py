df0192ddc507c929e8992bf72adbb585377c0c3f
Hongkuan Wang <hongkuanwang@Hongkuans-MacBook-Pro.local>
2020-08-13 11:05:13 +1000

    add 53. Maximum Subarray

---
+++ Array_Easy/Maximum_Subarray/Maximum_Subarray.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    test = Solution()
+    print(test.maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
